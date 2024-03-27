5f84ae9c29727b82a7672f7dd3ff91eebc76d574
dactam54 <tamphi67@gmail.com>
2024-03-27 16:19:29 +0700

    update

--- src/pages/public/Login.js
+++ src/pages/public/Login.js
@@ -77,50 +77,47 @@ const Login = (props) => {
       //   }));
       // }
     }
-
-
-    return (
-
-      <div className='w-main flex'>
-        <div className='w-1/3 flex-auto flex justify-center items-center'>
-        </div>
-        <div className='flex-auto flex flex-col justify-center gap-4 w-1/3'>
-          <h3 className='font-bold text-[24px] text-center'>Đăng nhập</h3>
-          <input
-            type="text"
-            className='p-2 bg-gray-100 border rounded-md placeholder:italic placeholder:text-gray-700'
-            placeholder='Nhập email'
-            value={payload.email}
-            onChange={e => setPayload(prev => ({ ...prev, email: e.target.value }))}
-          />
-          {/* {
-            error.errorEmail && <label className="errorLabel">{error.errorEmail}</label>
-          } */}
-
-          <input
-            type="password"
-            className='p-2 bg-gray-100 border rounded-md placeholder:italic placeholder:text-gray-700'
-            placeholder='Nhập mật khẩu'
-            value={payload.password}
-            onChange={e => setPayload(prev => ({ ...prev, password: e.target.value }))}
-          />
-          {/* {
-            error.errorPassword && <label className="errorLabel">{error.errorPassword}</label>
-          } */}
-          <button
-            type='button'
-            className='px-4 py-2 mx-auto bg-blue-500 text-white rounded-md font-semibold w-fit  '
-            onClick={handleSubmit}
-          >
-            Đăng nhập
-          </button>
-        </div>
-        <div className='w-1/3 flex-auto flex justify-center items-center'>
-        </div>
-      </div>
-
-    )
   }
+  return (
+    <div className='w-main flex'>
+      <div className='w-1/3 flex-auto flex justify-center items-center'>
+      </div>
+      <div className='flex-auto flex flex-col justify-center gap-4 w-1/3'>
+        <h3 className='font-bold text-[24px] text-center'>Đăng nhập</h3>
+        <input
+          type="text"
+          className='p-2 bg-gray-100 border rounded-md placeholder:italic placeholder:text-gray-700'
+          placeholder='Nhập email'
+          value={payload.email}
+          onChange={e => setPayload(prev => ({ ...prev, email: e.target.value }))}
+        />
+        {/* {
+          error.errorEmail && <label className="errorLabel">{error.errorEmail}</label>
+        } */}
+
+        <input
+          type="password"
+          className='p-2 bg-gray-100 border rounded-md placeholder:italic placeholder:text-gray-700'
+          placeholder='Nhập mật khẩu'
+          value={payload.password}
+          onChange={e => setPayload(prev => ({ ...prev, password: e.target.value }))}
+        />
+        {/* {
+          error.errorPassword && <label className="errorLabel">{error.errorPassword}</label>
+        } */}
+        <button
+          type='button'
+          className='px-4 py-2 mx-auto bg-blue-500 text-white rounded-md font-semibold w-fit  '
+          onClick={handleSubmit}
+        >
+          Đăng nhập
+        </button>
+      </div>
+      <div className='w-1/3 flex-auto flex justify-center items-center'>
+      </div>
+    </div>
+
+  )
 
 }
 export default Login
